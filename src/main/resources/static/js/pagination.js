90a968b4a08615a1d4526c2cee4126273176b635
jinxichang066 <jinxichang066@163.com>
2022-06-16 18:13:34 +0800

    将监控页面分页代码单独抽出

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -1,5 +1,6 @@
 function initPagination() {
     let paginationUlEvent = document.getElementById("pagination-ul");
+    paginationUlEvent.innerHTML = '';
     let leftButton = document.createElement("button");
     let rightButton = document.createElement("button");
     let leftIcon = document.createElement("i");
@@ -11,10 +12,11 @@ function initPagination() {
     leftButton.appendChild(leftIcon);
     paginationUlEvent.appendChild(leftButton);
 
-    for(let j = 1;j<4;j++){
+    let totalPageNum = pageTotal();
+    for (let j = 1; j < totalPageNum + 1; j++) {
         let li = document.createElement("li");
         let a = document.createElement("a");
-        if(j == 1){
+        if (j === 1) {
             li.className = "li-checked";
             a.className = "a-checked";
         }
@@ -32,32 +34,31 @@ function initPagination() {
 
     let liEvent = document.getElementsByTagName("li");
     let aEvent = document.getElementsByTagName("a");
-    for(let i=0;i<liEvent.length;i++){
-        liEvent[i].onclick = function(e){
+    for (let i = 0; i < liEvent.length; i++) {
+        liEvent[i].onclick = function (e) {
             removeCheckedClass();
             liEvent[i].className = "li-checked";
             aEvent[i].className = "a-checked";
             clickIndex = i;
-            console.log(i,liEvent.length-1)
-            switch (i){
-                case 0:
-                {
+            //展示选定页数据
+            playPagination(i + 1);
+            console.log("pageNum:" + (i + 1));
+            switch (i) {
+                case 0: {
                     leftButton.style.cursor = "not-allowed";
                     rightButton.style.cursor = "pointer";
                     leftButton.className = "noSelect";
                     rightButton.className = "isSelect";
                 }
                     break;
-                case (liEvent.length - 1):
-                {
+                case (liEvent.length - 1): {
                     leftButton.style.cursor = "pointer";
                     rightButton.style.cursor = "not-allowed";
                     leftButton.className = "isSelect";
                     rightButton.className = "noSelect";
                 }
                     break;
-                default:
-                {
+                default: {
                     leftButton.style.cursor = "pointer";
                     rightButton.style.cursor = "pointer";
                     leftButton.className = "isSelect";
@@ -67,40 +68,47 @@ function initPagination() {
         }
     }
 
-    function removeCheckedClass(){
-        for(let i = 0;i<liEvent.length;i++){
-            if(liEvent[i].className == "li-checked"){
+    function pageTotal() {
+        if (pageSize !== 0 && totalCount % pageSize === 0) {
+            return totalCount / pageSize
+        } else if (pageSize !== 0 && totalCount % pageSize !== 0) {
+            return Math.ceil(totalCount / pageSize);
+        } else return 0;
+    }
+
+    function removeCheckedClass() {
+        for (let i = 0; i < liEvent.length; i++) {
+            if (liEvent[i].className == "li-checked") {
                 liEvent[i].className = "";
                 aEvent[i].className = "";
             }
         }
     }
 
-    function clickButtonEvent(i){
-        console.log(i)
+    function clickButtonEvent(i) {
+        //展示上一页/下一页数据
+        playPagination(i + 1);
+        console.log("pageNum:" + (i + 1));
         removeCheckedClass();
         liEvent[i].className = "li-checked";
         aEvent[i].className = "a-checked";
         clickIndex = i;
-        switch (i){
-            case 0:
-            {
+        switch (i) {
+            case 0: {
                 leftButton.style.cursor = "not-allowed";
                 rightButton.style.cursor = "pointer";
                 leftButton.className = "noSelect";
                 rightButton.className = "isSelect";
             }
                 break;
-            case (liEvent.length - 1):
-            {
+            case (liEvent.length - 1): {
                 leftButton.style.cursor = "pointer";
                 rightButton.style.cursor = "not-allowed";
                 leftButton.className = "isSelect";
                 rightButton.className = "noSelect";
             }
                 break;
-            default:
-            {
+            default: {
                 leftButton.style.cursor = "pointer";
                 rightButton.style.cursor = "pointer";
                 leftButton.className = "isSelect";
@@ -111,12 +119,12 @@ function initPagination() {
 
     leftButton.onclick = function () {
         console.log(leftButton.style)
-        if(leftButton.style.cursor == "not-allowed"){
+        if (leftButton.style.cursor == "not-allowed") {
             return false;
-        }else if(leftButton.style.cursor == "pointer"){
-            if(clickIndex <= 0){
+        } else if (leftButton.style.cursor == "pointer") {
+            if (clickIndex <= 0) {
                 return false;
-            }else{
+            } else {
                 let index = clickIndex - 1;
                 clickButtonEvent(index);
             }
@@ -124,12 +132,12 @@ function initPagination() {
     }
 
     rightButton.onclick = function () {
-        if(rightButton.style.cursor == "not-allowed"){
+        if (rightButton.style.cursor == "not-allowed") {
             return false;
-        }else if(rightButton.style.cursor == "pointer"){
-            if(clickIndex >= liEvent.length-1){
+        } else if (rightButton.style.cursor == "pointer") {
+            if (clickIndex >= liEvent.length - 1) {
                 return false;
-            }else{
+            } else {
                 let index = clickIndex + 1;
                 clickButtonEvent(index);
             }
