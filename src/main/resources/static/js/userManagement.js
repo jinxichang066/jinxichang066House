ed0ec8b1e4f5048516cdefe1df07fc65cd31bb16
jinxichang066 <jinxichang066@163.com>
2022-07-04 01:05:39 +0800

    角色状态

--- src/main/resources/static/js/userManagement.js
+++ src/main/resources/static/js/userManagement.js
@@ -24,3 +24,11 @@ const roleKeyList = [
     "系统用户",
     "游客用户"
 ]
+
+// 此处一定要和后台Role.java中的枚举类定义一致
+// 跟上边的roleKeyOptions也一样
+const roleKeyStyleMap = new Map();
+roleKeyStyleMap.set("系统管理员", "btn btn-sm btn-outline-primary btn-round px-4 btn-width");
+roleKeyStyleMap.set("厂家管理员", "btn btn-sm btn-outline-light btn-round px-4 btn-width");
+roleKeyStyleMap.set("系统用户", "btn btn-sm btn-outline-success btn-round px-4 btn-width");
+roleKeyStyleMap.set("游客用户", "btn btn-sm btn-outline-theme btn-round px-4 btn-width");
